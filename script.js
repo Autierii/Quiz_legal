65822344fb1609f3f44797a881653f74b23ea424
RafaelAutieri <rafael_autieri@hotmail.com>
2023-08-09 17:25:44 -0300

    cores mais bonitas mas errada

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+// Obtendo elementos do DOM
 const questionContainer = document.querySelector('.question-container');
 const questionElement = document.querySelector('.question');
 const options = document.querySelectorAll('.option');
 const nextButton = document.getElementById('next-btn');
 
+// Array de perguntas e respostas
 const quizData = [
   {
     question: 'Quem é o deus do trovão na mitologia nórdica?',
@@ -31,66 +33,58 @@ const quizData = [
   }
 ];
 
-let currentQuestion = 0;
-let userAnswer = null;
+let currentQuestion = 0;  // Índice da pergunta atual
 
+// Função para carregar a pergunta atual
 function loadQuestion() {
   const question = quizData[currentQuestion];
   questionElement.textContent = question.question;
 
   options.forEach((option, index) => {
     option.textContent = question.options[index];
-    option.classList.remove('selected', 'correct');
-    option.addEventListener('click', selectAnswer);
+    option.classList.remove('correct-option');  // Remover classe de resposta correta
+    option.addEventListener('click', checkAnswer);
   });
 
-  nextButton.style.display = 'none';
+  nextButton.style.display = 'none';  // Esconder botão Próxima Pergunta
 }
 
-function selectAnswer(e) {
-  options.forEach(option => {
-    option.classList.remove('selected');
-  });
+// Função para verificar a resposta do usuário
+function checkAnswer(e) {
+  const selectedOption = e.target;
+  const correctAnswer = quizData[currentQuestion].answer;
 
-  e.target.classList.add('selected');
-  userAnswer = e.target.textContent;
-  nextButton.style.display = 'block';
-}
-
-function showCorrectAnswer() {
-  options.forEach(option => {
-    if (option.textContent === quizData[currentQuestion].answer) {
-      option.classList.add('correct');
-    }
-  });
-}
-
-function nextQuestion() {
-  if (userAnswer === quizData[currentQuestion].answer) {
+  if (selectedOption.textContent === correctAnswer) {
+    selectedOption.style.backgroundColor = '#27AE60';  // Resposta correta (verde)
+  } else {
+    selectedOption.style.backgroundColor = '#E74C3C';  // Resposta incorreta (vermelho)
     options.forEach(option => {
-      if (option.textContent === userAnswer) {
-        option.classList.add('correct');
+      if (option.textContent === correctAnswer) {
+        option.classList.add('correct-option');  // Adicionar classe de resposta correta
       }
     });
-  } else {
-    showCorrectAnswer();
   }
 
   options.forEach(option => {
-    option.removeEventListener('click', selectAnswer);
+    option.removeEventListener('click', checkAnswer);  // Impedir seleção adicional
   });
 
-  nextButton.style.display = 'none';
+  nextButton.style.display = 'block';  // Exibir botão Próxima Pergunta
+}
 
-  currentQuestion++;
+// Função para avançar para a próxima pergunta
+function nextQuestion() {
+  currentQuestion++;  // Avançar para a próxima pergunta
 
   if (currentQuestion < quizData.length) {
-    loadQuestion();
+    loadQuestion();  // Carregar a próxima pergunta
+    nextButton.style.display = 'none';  // Esconder botão Próxima Pergunta
   } else {
-    questionContainer.innerHTML = '<h2>Quiz Concluído!</h2>';
+    questionContainer.innerHTML = '<h2>Quiz Concluído!</h2>';  // Exibir mensagem de conclusão
   }
 }
 
+// Adicionar evento de clique ao botão Próxima Pergunta
 nextButton.addEventListener('click', nextQuestion);
 
-loadQuestion();
+loadQuestion();  // Carregar a primeira pergunta
